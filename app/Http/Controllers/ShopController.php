989415749d4b7751c781c3a6cd5006c54a54f962
Xpressglobe <tobeodiachi@yahoo.com>
2022-12-15 11:39:56 +0100

    cart

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -64,9 +64,12 @@ class ShopController extends Controller
         // $wishlist = Wishlist::where('user_id', auth()->user()->id)->pluck('id')->toArray();
         
         // where('slug', '!=', $product->slug)->
+        // dd($user);
         if(!is_null($user)){
             // $user->wishlist->products;
+                if(!is_null($user->wishlist->products()))
                 $mightLike = $user->wishlist->products()->orWhere('category_id', $product->category_id)->mightAlsoLike()->get();
+                $mightLike = Product::where('slug', '!=', $product->slug)->orWhere('category_id', $product->category_id)->mightAlsoLike()->get();
         }
         else{
             $mightLike = Product::where('slug', '!=', $product->slug)->orWhere('category_id', $product->category_id)->mightAlsoLike()->get();
