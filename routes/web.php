a2bcb46a94b6e2974963b938e0694a29444db953
Xpressglobe <tobeodiachi@yahoo.com>
2023-01-28 10:19:59 +0100

    Exception handling

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/product/{slug}', ProductController::class, 'show')->name('shop.show');
 Route::get('404', function () {
-    return view('errors.500');
+    return view('errors.404');
 })->name('404');
 Route::middleware(['auth'])->group(function () {
 
